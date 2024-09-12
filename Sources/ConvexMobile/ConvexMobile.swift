d6dbe79fa2dfc66cbb3e9473cc1543b1795e8139
Christian Wyglendowski <christian@dowski.com>
2024-09-12 12:37:39 -0400

    Add support for mutation and passing Int, Double, String and Bool as arguments

--- Sources/ConvexMobile/ConvexMobile.swift
+++ Sources/ConvexMobile/ConvexMobile.swift
@@ -16,15 +16,32 @@ public class ConvexClient {
     self.ffiClient = ffiClient
   }
 
-  public func subscribe<T: Decodable>(name: String) async throws -> AnyPublisher<T, ClientError> {
+  public func subscribe<T: Decodable>(name: String, args: [String: ConvexEncodable?]? = nil)
+    async throws
+    -> AnyPublisher<T, ClientError>
+  {
     let publisher = PassthroughSubject<T, ClientError>()
     let adapter = SubscriptionAdapter<T>(publisher: publisher)
     let cancellationHandle = try await ffiClient.subscribe(
-      name: name, args: [:], subscriber: adapter)
+      name: name,
+      args: args?.mapValues({ v in
+        try v?.convexEncode() ?? "null"
+      }) ?? [:], subscriber: adapter)
     return publisher.handleEvents(receiveCancel: {
       cancellationHandle.cancel()
     })
     .eraseToAnyPublisher()
+  }
+
+  public func mutation<T: Decodable>(name: String, args: [String: ConvexEncodable?]? = nil)
+    async throws -> T
+  {
+    let rawResult = try await ffiClient.mutation(
+      name: name,
+      args: args?.mapValues({ v in
+        try v?.convexEncode() ?? "null"
+      }) ?? [:])
+    return try! JSONDecoder().decode(T.self, from: Data(rawResult.utf8))
   }
 }
 
@@ -68,3 +85,34 @@ public struct ConvexInt<IntegerType: FixedWidthInteger>: Decodable {
     case integer = "$integer"
   }
 }
+
+struct ConvexBase64Int: Decodable {
+  let integer: String
+
+  enum ConvexTypeKey: String, CodingKey {
+    case integer = "$integer"
+  }
+}
+
+public protocol ConvexEncodable {
+  func convexEncode() throws -> String
+}
+
+extension ConvexEncodable where Self: FixedWidthInteger, Self: Encodable {
+  public func convexEncode() throws -> String {
+    let data = withUnsafeBytes(of: self) { Data($0) }
+    return String(
+      decoding: try JSONEncoder().encode(["$integer": data.base64EncodedString()]), as: UTF8.self)
+  }
+}
+
+extension ConvexEncodable where Self: Encodable {
+  public func convexEncode() throws -> String {
+    return String(decoding: try JSONEncoder().encode(self), as: UTF8.self)
+  }
+}
+
+extension Int: ConvexEncodable {}
+extension Double: ConvexEncodable {}
+extension Bool: ConvexEncodable {}
+extension String: ConvexEncodable {}
--- Tests/ConvexMobileTests/ConvexMobileTests.swift
+++ Tests/ConvexMobileTests/ConvexMobileTests.swift
@@ -4,7 +4,7 @@ import XCTest
 @testable import UniFFI
 
 final class ConvexMobileTests: XCTestCase {
-  func testSubscribe() async throws {
+  func testSubscribeResult() async throws {
     let expectation = self.expectation(description: "subscribe")
     var error: ClientError?
     var result: Message?
@@ -32,7 +32,59 @@ final class ConvexMobileTests: XCTestCase {
     XCTAssertEqual(error, nil)
   }
 
-  func testCancellation() async throws {
+  func testMissingSubscribeArgs() async throws {
+    let expectation = self.expectation(description: "subscribe")
+    let fakeFfiClient = FakeMobileConvexClient()
+    let client = ConvexMobile.ConvexClient(ffiClient: fakeFfiClient)
+
+    let cancellationHandle = try await client.subscribe(name: "foo").sink(
+      receiveCompletion: { completion in
+        switch completion {
+        case .finished:
+          break
+        case .failure(let clientError):
+          break
+        }
+      },
+      receiveValue: { (value: Message) in
+        expectation.fulfill()
+      })
+
+    await fulfillment(of: [expectation], timeout: 10)
+
+    XCTAssertEqual(fakeFfiClient.subscriptionArgs, [:])
+  }
+
+  func testPopulatedSubscribeArgs() async throws {
+    let expectation = self.expectation(description: "subscribe")
+    let fakeFfiClient = FakeMobileConvexClient()
+    let client = ConvexMobile.ConvexClient(ffiClient: fakeFfiClient)
+
+    let cancellationHandle = try await client.subscribe(
+      name: "foo", args: ["aString": "bar", "aDouble": 42.0, "anInt": 42, "aNil": nil]
+    ).sink(
+      receiveCompletion: { completion in
+        switch completion {
+        case .finished:
+          break
+        case .failure(let clientError):
+          break
+        }
+      },
+      receiveValue: { (value: Message) in
+        expectation.fulfill()
+      })
+
+    await fulfillment(of: [expectation], timeout: 10)
+
+    XCTAssertEqual(fakeFfiClient.subscriptionArgs["aString"], "\"bar\"")
+    XCTAssertEqual(fakeFfiClient.subscriptionArgs["aDouble"], "42")
+    XCTAssertEqual(fakeFfiClient.subscriptionArgs["anInt"], "{\"$integer\":\"KgAAAAAAAAA=\"}")
+    XCTAssertEqual(fakeFfiClient.subscriptionArgs["aNil"], "null")
+
+  }
+
+  func testSubscribeCancellation() async throws {
     let expectation = self.expectation(description: "subscribe")
     var error: ClientError?
     let ffiClient = FakeMobileConvexClient()
@@ -58,16 +110,29 @@ final class ConvexMobileTests: XCTestCase {
     XCTAssertEqual(ffiClient.cancellationCount, 1)
     XCTAssertEqual(error, nil)
   }
+
+  func testMutationRoundTrip() async throws {
+    let fakeFfiClient = FakeMobileConvexClient()
+    let client = ConvexMobile.ConvexClient(ffiClient: fakeFfiClient)
+
+    let message: Message = try await client.mutation(name: "foo", args: ["anInt": 101])
+
+    XCTAssertEqual(message.id, "the_id")
+    XCTAssertEqual(message.val, 101)
+
+  }
 }
 
 class FakeMobileConvexClient: UniFFI.MobileConvexClientProtocol {
   var cancellationCount = 0
+  var subscriptionArgs: [String: String] = [:]
   func action(name: String, args: [String: String]) async throws -> String {
     return "foo"
   }
 
   func mutation(name: String, args: [String: String]) async throws -> String {
-    return "foo"
+    let receivedConvexInt = args["anInt"]!
+    return "{\"_id\": \"the_id\", \"val\": \(receivedConvexInt), \"extra\": null}"
   }
 
   func query(name: String, args: [String: String]) async throws -> String {
@@ -81,6 +146,7 @@ class FakeMobileConvexClient: UniFFI.MobileConvexClientProtocol {
   func subscribe(name: String, args: [String: String], subscriber: any UniFFI.QuerySubscriber)
     async throws -> UniFFI.SubscriptionHandle
   {
+    subscriptionArgs = args
     let _ = Task {
       try await Task.sleep(nanoseconds: UInt64(0.05 * 1_000_000_000))
       subscriber.onUpdate(
