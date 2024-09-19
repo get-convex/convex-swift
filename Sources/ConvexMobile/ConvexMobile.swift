76eeb344a4fcd7692978d7402b52704161bfe74f
Christian Wyglendowski <christian@dowski.com>
2024-09-19 14:33:32 -0400

    Adds some API documentation

--- Sources/ConvexMobile/ConvexMobile.swift
+++ Sources/ConvexMobile/ConvexMobile.swift
@@ -5,9 +5,21 @@ import Combine
 import Foundation
 @_exported import UniFFI
 
+/// A client API for interacting with a Convex backend.
+///
+/// Handles marshalling of data between calling code and the
+/// [convex-mobile](https://github.com/get-convex/convex-mobile) and
+/// [convex-rs](https://github.com/get-convex/convex-rs) native libraries.
+///
+/// Consumers of this client should use Swift's ``Decodable``  protocol for handling data received from the
+/// Convex backend.
 public class ConvexClient {
   let ffiClient: UniFFI.MobileConvexClientProtocol
 
+  /// Creates a new instance of ``ConvexClient``.
+  ///
+  /// - Parameters:
+  ///   - deploymentUrl: The Convex backend URL to connect to; find it in the [dashboard](https://dashboard.convex.dev) Settings for your project
   public init(deploymentUrl: String) {
     self.ffiClient = UniFFI.MobileConvexClient(deploymentUrl: deploymentUrl)
   }
@@ -16,6 +28,15 @@ public class ConvexClient {
     self.ffiClient = ffiClient
   }
 
+  /// Subscribes to the query with the given `name` and converts data from the subscription into an
+  /// ``AnyPublisher<T, ClientError>``.
+  ///
+  /// The upstream Convex subscription will be canceled if whatever is subscribed to returned publisher
+  /// stops listening.
+  ///
+  /// - Parameters:
+  ///   - name: A value in "module:query_name"  format that will be used when calling the backend
+  ///   - args: An optional ``Dictionary`` of arguments to be sent to the backend query function
   public func subscribe<T: Decodable>(name: String, args: [String: ConvexEncodable?]? = nil)
     throws
     -> AnyPublisher<T, ClientError>
@@ -45,12 +66,26 @@ public class ConvexClient {
     .eraseToAnyPublisher()
   }
 
+  /// Executes the mutation with the given `name` and `args` and returns the result.
+  ///
+  /// For mutations that don't return a value, prefer calling the version of this method that doesn't return a value.
+  ///
+  /// - Parameters:
+  ///   - name: A value in "module:mutation_name"  format that will be used when calling the backend
+  ///   - args: An optional ``Dictionary`` of arguments to be sent to the backend mutation function
   public func mutation<T: Decodable>(name: String, args: [String: ConvexEncodable?]? = nil)
     async throws -> T
   {
     return try await mutationForResult(name: name, args: args)
   }
 
+  /// Executes the mutation with the given `name` and `args` without returning a result.
+  ///
+  /// For mutations that return a value, prefer calling the version of this method that returns a ``Decodable`` value.
+  ///
+  /// - Parameters:
+  ///   - name: A value in "module:mutation_name"  format that will be used when calling the backend
+  ///   - args: An optional ``Dictionary`` of arguments to be sent to the backend mutation function
   public func mutation(name: String, args: [String: ConvexEncodable?]? = nil)
     async throws
   {
@@ -71,26 +106,52 @@ public class ConvexClient {
   }
 }
 
+/// Authentication states that can be experienced when using an ``AuthProvider`` with
+/// ``ConvexClientWithAuth``.
 public enum AuthState<T> {
+  /// Represents an authenticated user.
+  ///
+  /// Contains authentication data from the associated ``AuthProvider``.
   case authenticated(T)
+  /// Represents an unauthenticated user.
   case unauthenticated
+  /// Represents an ongoing authentication attempt.
   case loading
 }
 
+/// An authentication provider, used with ``ConvexClientWithAuth``.
+///
+/// The generic type `T` is the data returned by the provider upon a successful authentication attempt.
 public protocol AuthProvider<T> {
   associatedtype T
 
+  /// Trigger a login flow, which might launch a new UI/screen.
   func login() async throws -> T
+  /// Trigger a logout flow, which might launch a new UI/screen.
   func logout() async throws
+  /// Trigger a cached, UI-less re-authentication ussing stored credentials from a previous ``login()``.
   func loginFromCache() async throws -> T
+  /// Extracts a [JWT ID token](https://openid.net/specs/openid-connect-core-1_0.html#IDToken)
+  /// from the `authResult`.
   func extractIdToken(authResult: T) -> String
 }
 
+/// Like ``ConvexClient``, but supports integration with an authentication provider via ``AuthProvider``.
+///
+/// The generic parameter `T` matches the type of data returned by the ``AuthProvider`` upon successful
+/// authentication.
 public class ConvexClientWithAuth<T>: ConvexClient {
   private let authPublisher = CurrentValueSubject<AuthState<T>, Never>(AuthState.unauthenticated)
-  public let authState: AnyPublisher<AuthState<T>, Never>
   private let authProvider: any AuthProvider<T>
 
+  /// A publisher that updates with the current ``AuthState`` of this client instance.
+  public let authState: AnyPublisher<AuthState<T>, Never>
+
+  /// Creates a new instance of ``ConvexClientWithAuth``.
+  ///
+  /// - Parameters:
+  ///   - deploymentUrl: The Convex backend URL to connect to; find it in the [dashboard](https://dashboard.convex.dev) Settings for your project
+  ///   - authProvider: An instance that will handle the actual authentication duties.
   public init(deploymentUrl: String, authProvider: any AuthProvider<T>) {
     self.authProvider = authProvider
     self.authState = authPublisher.eraseToAnyPublisher()
@@ -103,14 +164,32 @@ public class ConvexClientWithAuth<T>: ConvexClient {
     super.init(ffiClient: ffiClient)
   }
 
+  /// Triggers a UI driven login flow and updates the ``authState``.
+  ///
+  /// The ``authState`` is set to ``AuthState.loading`` immediately upon calling this method and
+  /// will change to either ``AuthState.authenticated`` or ``AuthState.unauthenticated``
+  /// depending on the result.
   public func login() async {
     await login(strategy: authProvider.login)
   }
 
+  /// Triggers a cached, UI-less re-authentication flow using previously stored credentials and updates the
+  /// ``authState``.
+  ///
+  /// If no credentials were previously stored, or if there is an error reusing stored credentials, the resulting
+  /// ``authState`` willl be ``AuthState.unauthenticated``. If supported by the ``AuthProvider``,
+  /// a call to ``login()`` should store another set of credntials upon successful authentication.
+  ///
+  /// The ``authState`` is set to ``AuthState.loading`` immediately upon calling this method and
+  /// will change to either ``AuthState.authenticated`` or ``AuthState.unauthenticated``
+  /// depending on the result.
   public func loginFromCache() async {
     await login(strategy: authProvider.loginFromCache)
   }
 
+  /// Triggers a logout flow and updates the ``authState``.
+  ///
+  /// The ``authState`` will change to ``AuthState.unauthenticated`` if logout is successful.
   public func logout() async {
     do {
       try await authProvider.logout()
